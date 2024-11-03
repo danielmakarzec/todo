978a479037bf61d1bb0670501f0f338c87a5b978
danielmakarzec <danielmakarzec@gmail.com>
2024-11-03 22:10:22 +0000

    fix1

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('static').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192x192.jpeg',
-        '/icon-512x512.jpeg',
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192x192.jpeg',
+        './icon-512x512.jpeg',
       ])
     })
   )
